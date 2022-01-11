5104e2c3297d75eec062951b14735e1dc4183db0
tamat <tamat@LAPTOP-LUUND0DS>
2022-01-11 08:38:02 +0530

    fifth commit

---
+++ src/jo45/hello.java
@@ -0,0 +1,10 @@
+package jo45;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		system.out.println("hello bhavi")
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module jo2 {
+}
